35b73b66dd4f768e08d6f41862f4df8003ca5b9c
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-05-31 13:35:07 -0400

    exported form to appjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
+    <Form />
 
-      
     </div>
   );
 }
