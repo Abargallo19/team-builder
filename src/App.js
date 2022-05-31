a857a9671daf0e5ee55476835602f9590e00e168
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-05-31 13:41:34 -0400

    added axios and setup initial state for form values

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import Form from './Form';
 import './App.css';
 
+const initialFormValues = {
+
+}
+
+
+
 function App() {
+
+  const [formValues, setFormValues] = useState(null)
+
   return (
     <div className="App">
       
